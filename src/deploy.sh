15e1bd3045c93f28f3af8be695bf58c5a062b291
Viktor <cotres@mail.ru>
2025-03-09 18:53:34 +0300

    change deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,8 +7,6 @@ REMOTE_USER="viktor"
 REMOTE_HOST="192.168.1.74"   
 REMOTE_DIR="/usr/local/bin"  
 
-chmod +x $S21_CAT_PATH $S21_GREP_PATH
-
 scp $S21_CAT_PATH $S21_GREP_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
 
 if [ $? -eq 0 ]; then
